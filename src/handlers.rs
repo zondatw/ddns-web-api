782ada51378185dbe9ed232dd504387ee2c1ac52
zonda yang <u226699@gmail.com>
2022-09-25 08:49:39 +0800

    Add response's serializer

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,9 +1,30 @@
-use actix_web::{post, web, Responder};
+use actix_web::{ body::BoxBody, http::header::ContentType, post, web, HttpRequest, HttpResponse, Responder};
+use serde::Serialize;
 
 use crate::constants::DNSState;
+
+#[derive(Serialize)]
+struct DDNSSerializer {
+    dns_key: String,
+}
+
+impl Responder for DDNSSerializer {
+    type Body = BoxBody;
+
+    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
+    }
+}
 
 #[post("/ddns")]
 pub async fn handler_ddns_set(data: web::Data<DNSState>) -> impl Responder {
     let dns_key = &data.dns_key;
-    format!("DDNS dns key {dns_key}!")
+
+    DDNSSerializer {
+        dns_key: dns_key.clone()
+    }
 }
