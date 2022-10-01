01dffead7551b5b84b8c398e6e0ab0f4cde541a9
zonda yang <u226699@gmail.com>
2022-10-01 10:13:54 +0800

    Add api base routing

--- src/api/routes.rs
+++ src/api/routes.rs
@@ -4,5 +4,9 @@ use actix_web::{web};
 use super::ddns;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.configure(ddns::routes::config);
+    cfg.service(
+        web::scope("/api").configure(
+            ddns::routes::config
+        )
+    );
 }
