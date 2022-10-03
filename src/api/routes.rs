966ea5c1267a7b909b6ddade94c6918f26bf20ca
zonda yang <u226699@gmail.com>
2022-10-04 07:58:25 +0800

    Move dns_key config to ddns module's routes

--- src/api/routes.rs
+++ src/api/routes.rs
@@ -1,7 +1,12 @@
 use actix_web::web;
 
 use super::ddns;
+use crate::config;
+use crate::core::constants::DNSState;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/api").configure(ddns::routes::config));
+    cfg.app_data(web::Data::new(DNSState {
+        dns_key: (*config::base::dns_key().lock().unwrap()).clone(),
+    }))
+    .service(web::scope("/api").configure(ddns::routes::config));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use actix_web::{middleware::Logger, web, App, HttpServer};
+use actix_web::{middleware::Logger, App, HttpServer};
 
 mod api;
 mod config;
 mod core;
-
-use crate::core::constants::DNSState;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -16,9 +14,6 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(Logger::default())
             .wrap(Logger::new("%a %{User-Agent}i"))
-            .app_data(web::Data::new(DNSState {
-                dns_key: (*config::base::dns_key().lock().unwrap()).clone(),
-            }))
             .configure(api::routes::config)
     })
     .bind(("127.0.0.1", 8080))?
