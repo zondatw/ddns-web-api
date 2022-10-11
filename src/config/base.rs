1d2070b4d473d90a50bc18275c6ae7dbce1a779a
zonda yang <u226699@gmail.com>
2022-10-11 23:46:19 +0800

    Add config_key_wrapper marco

--- src/config/base.rs
+++ src/config/base.rs
@@ -1,18 +1,27 @@
 use dotenv::dotenv;
+use paste::paste;
 use std::borrow::BorrowMut;
 use std::sync::{Mutex, Once};
 
-static mut STD_ONCE_DNS_KEY: Option<Mutex<String>> = None;
-static DNS_KEY_INIT: Once = Once::new();
+macro_rules! config_key_wrapper {
+    ($name:ident, $variable_name:ident) => {
+        paste! {
+            static mut [<STD_ONCE_ $name>]: Option<Mutex<String>> = None;
+            static [<$name _INIT>]: Once = Once::new();
 
-pub fn dns_key<'a>() -> &'a Mutex<String> {
-    DNS_KEY_INIT.call_once(|| unsafe {
-        *STD_ONCE_DNS_KEY.borrow_mut() = Some(Mutex::new(
-            std::env::var("DNS_KEY").expect("DNS_KEY must be set."),
-        ));
-    });
-    unsafe { STD_ONCE_DNS_KEY.as_ref().unwrap() }
+            pub fn $variable_name<'a>() -> &'a Mutex<String> {
+                [<$name _INIT>].call_once(|| unsafe {
+                    *[<STD_ONCE_ $name>].borrow_mut() = Some(Mutex::new(
+                        std::env::var(stringify!($name)).expect(format!("{:?} must be set.", stringify!($name)).as_str()),
+                    ));
+                });
+                unsafe { [<STD_ONCE_ $name>].as_ref().unwrap() }
+            }
+        }
+    };
 }
+
+config_key_wrapper!(DNS_KEY, dns_key);
 
 pub fn init() {
     dotenv().ok();
